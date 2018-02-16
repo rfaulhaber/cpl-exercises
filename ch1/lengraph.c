8f9cb2c465555d003247b871f2be51cdee2d78f8
Ryan Faulhaber <faulhaber.14@osu.edu>
2018-02-16 14:36:50 -0500

    length histograph written

--- ch1/lengraph.c
+++ ch1/lengraph.c
@@ -1,3 +1,40 @@
 #include <stdio.h>
 
+#define OUT 0
+#define IN  1
 
+main() {
+	int lens[20];
+
+	for (int i = 0; i < 20; i++) {
+		lens[i] = 0;
+	}
+
+	int wl = 0;
+
+	int c;
+
+	while ((c = getchar()) != EOF) {
+		if (c == ' ' || c == '\t' || c == '\n') {
+			if (wl < 21) {
+				++lens[wl];
+			}
+
+			wl = 0;
+		} else {
+			++wl;
+		}
+	}
+
+	for (int i = 0; i < 20; i++) {
+		if (lens[i] > 0) {
+			printf("%i >", i);
+			for (int j = 0; j < lens[i]; j++) {
+				printf("%s", "|");
+			}
+
+			printf("%s", "\n");
+		}
+
+	}
+}
